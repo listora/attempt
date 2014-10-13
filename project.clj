2c955c83e4d335aea0ecbda6a8caede082e9a2b4
James Reeves <jreeves@weavejester.com>
2014-10-13 16:37:45 +0100

    Add URL and description to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject listora/attempt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Asynchronously evaluate functions with retries and fallback"
+  :url "https://github.com/listora/attempt"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
