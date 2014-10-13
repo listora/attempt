db04b7bcfd19e27ad63185d02412f892e39f07cc
James Reeves <jreeves@weavejester.com>
2014-10-13 14:35:20 +0100

    Add async thunk processing

--- src/listora/attempt.clj
+++ src/listora/attempt.clj
@@ -1,6 +1,25 @@
-(ns listora.attempt)
+(ns listora.attempt
+  (:require [clojure.core.async :as a :refer [<! go go-loop thread]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def channel
+  (a/chan (a/buffer 1024)))
+
+(defn- process-attempt [{:keys [thunk]}]
+  (thread (thunk)))
+
+(defn- run-attempts []
+  (go-loop []
+    (when-let [attempt (<! channel)]
+      (process-attempt attempt)
+      (recur))))
+
+(let [run (delay (run-attempts))]
+  (defn startup-attempts []
+    (deref run)))
+
+(defn shutdown-attempts []
+  (a/close! channel))
+
+(defn attempt [thunk & [{:as options}]]
+  (startup-attempts)
+  (a/put! channel {:thunk thunk}))
