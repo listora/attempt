1d49e374b6f1ea5bcc1f96672ff9aa5f67d0bc4e
James Reeves <jreeves@weavejester.com>
2014-10-13 14:56:25 +0100

    Add fallback for exceptions

--- src/listora/attempt.clj
+++ src/listora/attempt.clj
@@ -4,9 +4,15 @@
 (def channel
   (a/chan (a/buffer 1024)))
 
-(defn- process-attempt [{:keys [thunk return]}]
-  (go (let [value (<! (thread (thunk)))]
-        (return value))))
+(defn- run-thunk [thunk]
+  (try {:return (thunk)}
+       (catch Exception e {:error e})))
+
+(defn- process-attempt [{:keys [thunk return fallback]}]
+  (go (let [result (<! (thread (run-thunk thunk)))]
+        (if-let [ret (:return result)]
+          (return ret)
+          (return fallback)))))
 
 (defn- run-attempts []
   (go-loop []
@@ -21,8 +27,12 @@
 (defn shutdown-attempts []
   (a/close! channel))
 
+(def default-options
+  {:fallback nil})
+
 (defn attempt [thunk & [{:as options}]]
   (startup-attempts)
-  (let [return (promise)]
-    (a/put! channel {:thunk thunk, :return return})
+  (let [options (merge default-options options)
+        return  (promise)]
+    (a/put! channel (assoc options :thunk thunk, :return return))
     return))
